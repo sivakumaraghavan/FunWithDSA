16ee70c55963adde7a235a311ce302a98682b34a
Sivakumar G.R <siva.anaz@gmail.com>
2020-07-30 22:43:23 +0530

    Initial commit for data structures and algorithms

---
+++ Algorithms/Program.cs
@@ -0,0 +1,15 @@
+﻿using Algorithms.Searching;
+using System;
+
+namespace FunWithDSA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Algorithms");
+            //BinarySearch bs = new BinarySearch();
+            //QuickSort qs = new QuickSort();
+        }
+    }
+}
---
+++ Algorithms/Sorting/QuickSort.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.Sorting
+{
+    class QuickSort
+    {
+        public QuickSort()
+        {
+            Console.WriteLine("Enter the elements of an array (space separated)");
+            var input = Console.ReadLine();
+            string[] sInput = input.Split(' ');
+            int[] iInput = Array.ConvertAll(sInput, int.Parse);
+
+            Console.WriteLine("******Quick Sort*******");
+
+        }
+    }
+}
