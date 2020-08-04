68f0f29ef038ec2af7b92acf7af995a8afc1f1b1
sivakumaraghavan <sivkumaraghavan@gmail.com>
2020-08-04 17:58:15 +0530

    Added recursion class and implemente factorial, fibonacci

--- Algorithms/Recursion/Recursion.cs
+++ Algorithms/Recursion/Recursion.cs
@@ -6,5 +6,33 @@ namespace Algorithms.Recursion
 {
     class Recursion
     {
+        public Recursion()
+        {
+            Console.WriteLine("Enter the number to find factorial and fibonacci using recursion");
+
+            int number = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Recursion --> Factorial");
+            Console.WriteLine(Factorial(number));
+
+            Console.WriteLine("Recursion --> Fabonacci");
+            Console.WriteLine(Fibonacci(number));
+        }
+
+        private int Factorial(int number)
+        {
+            if (number <= 1)
+                return number;
+
+            return number * Factorial(number - 1);
+        }
+
+        private int Fibonacci(int number)
+        {
+            if (number <= 1)
+                return number;
+
+            return Fibonacci(number - 1) + Fibonacci(number - 2);
+        }
     }
 }
